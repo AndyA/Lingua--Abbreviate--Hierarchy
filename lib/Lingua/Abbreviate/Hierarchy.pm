b8332a383a9c4792bf60dfa4931ccab3f944f952
Andy Armstrong <andy@hexten.net>
2009-05-28 23:25:23 +0100

    Added

---
+++ lib/Lingua/Abbreviate/Hierarchy.pm
@@ -0,0 +1,62 @@
+package Lingua::Abbreviate::Hierarchy;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Lingua::Abbreviate::Hierarchy - [One line description of module's purpose here]
+
+=head1 VERSION
+
+This document describes Lingua::Abbreviate::Hierarchy version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Lingua::Abbreviate::Hierarchy;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< somfunc >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+Lingua::Abbreviate::Hierarchy requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-lingua-abbreviate-hierarchy@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2009, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'Lingua::Abbreviate::Hierarchy' );
+}
+
+diag( "Testing Lingua::Abbreviate::Hierarchy $Lingua::Abbreviate::Hierarchy::VERSION" );
